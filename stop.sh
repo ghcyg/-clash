ca0d1969e1bf6897d7b67654ea0608dea9b1fde8
ghcyg <68024957+ghcyg@users.noreply.github.com>
2023-04-05 18:04:53 +0800

    stop停止

---
+++ stop.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# 获取系统架构
+arch=$(uname -m)
+if [ "$arch" = "x86_64" ]; then
+	
+	# 自定义函数
+	Num=`ps | grep clash-linux-amd64 | grep -v grep | wc -l`;
+	if [ $Num -eq 0 ];then
+    		echo 进程已停止 
+    		exit
+	else
+    		echo 目标存在，将执行杀死
+		killall -9 clash-linux-amd64
+	fi
+elif [[ "$arch" =~ "aarch64" ||  "$arch" =~ "arm64" ]]; then
+	
+	abc=`ps | grep clash-linux-arm64 | grep -v grep | wc -l`;
+	if [ $abc -eq 0 ];then
+    		echo 进程已停止 
+	else
+    		echo 目标存在，将执行杀死
+		killall -9 clash-linux-arm64 
+	fi
+elif [ "$arch" = "armv7" ]; then
+      
+	
+	qaz=`ps | grep clash-linux-armv7 | grep -v grep | wc -l`;
+	if [ $qaz -eq 0 ];then
+    		echo 进程已停止 
+	else
+    		echo 目标存在，将执行杀死
+    		killall -9 clash-linux-armv7
+    	fi
+else
+    echo "不支持该系统，请手动停止"
+    break
+    
+fi
+sleep 2
+echo 做最后检查
+ikun=`ps | grep clash | grep -v grep | wc -l`;
+if [ $ikun -eq 0 ];then
+	sleep 1
+	echo 已停止，退出    
+	exit
+else
+	echo 失败，请手动尝试
+fi
